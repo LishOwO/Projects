c8e57dd788927f168fcf91e445cb2c98a69f2fe9
Lishian <waterpaix@gmail.com>
2024-11-15 19:11:23 +0100

    construit le squelette

--- Python/ProjetPremiere/main.py
+++ Python/ProjetPremiere/main.py
@@ -1,31 +1,29 @@
+import sys
 import pygame
-pygame.init()
-
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-
-player = pygame.Rect((300, 250, 50, 50))
-
-screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
-
-run = True
-
-while run:
-
-    
-
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-pygame.quit()
 
 class Game:
     def __init__(self):
+        pygame.init()
+
+        pygame.display.set_caption('Test game')
+
         self.SCREEN_WIDTH = 800
         self.SCREEN_HEIGHT = 600
 
-        self.screen = pygame.display.set_mode((self.SCREEN_WIDTH,SCREEN_HEIGHT))
+        self.clock = pygame.time.Clock()
 
-    pass
+        self.screen = pygame.display.set_mode((self.SCREEN_WIDTH,self.SCREEN_HEIGHT))
 
+        self.player = pygame.Rect((300, 250, 50, 50))
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.quit():
+                    pygame.quit()
+                    sys.exit()
+
+            pygame.display.update()
+            self.clock.tick(60)
+
+Game().run()
