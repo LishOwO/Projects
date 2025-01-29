55d996b5a4b5bae4e079824a3d91e5c6ee1f38b1
Vincent <61601267+LishOwO@users.noreply.github.com>
2025-01-29 14:18:00 +0100

    nouveau projet!

--- Python/morse-decoder/main.py
+++ Python/morse-decoder/main.py
@@ -1,1 +1,6 @@
 
+decoder_direction = int(input('Welcome to this morse decoder!\nType 1 for Morse to Alphabet, Type 2 for Alphabet to Morse\n1 or 2 :'))
+
+if decoder_direction == 1:
+    string_to_decode = input("Enter your string you want to decode using '.', '_', '/','//':\n")
+
